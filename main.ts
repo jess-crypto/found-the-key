5c672d950d0278f70de5efe6d12ce6b96762a7c6
jess-crypto <117653988+jess-crypto@users.noreply.github.com>
2022-11-07 10:16:25 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+print('pinNumber is: ' + pinNumber
+);
+print(foundKey === "yes", "&&" 'pinNumber' === 10),
+    print('You opened the lock!');
+if(foundKey === 'no' && pinNumber !==10){
+    print('The pin number is incorrect.');
+}
